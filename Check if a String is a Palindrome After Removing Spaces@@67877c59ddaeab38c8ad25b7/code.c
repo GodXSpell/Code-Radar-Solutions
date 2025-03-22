70efee0383abfc6447bd6b206764cadc8835ea8e
Tarunpreet Singh <141033706+GodXSpell@users.noreply.github.com>
2025-03-22 16:45:59 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

--- Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> // For strdup
+#include <stdbool.h>
+
+bool isPalindrome(char str[], int length) {
+    int left = 0, right = length - 1;
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false; 
+        }
+        left++;
+        right--;
+    }
+    return true; 
+}
 
 int main() {
-    char str[100];
-    scanf("%[^\n]", str); 
+    char str[100], noSpaces[100];
+    int index = 0;
 
-    char *token = strtok(str, " "); 
-    char *normie; 
-    int len, len2;
+    scanf("%[^\n]", str);
 
-    while (token != NULL) {
-        //printf("%s", token); 
-
-        normie = strdup(token); // Duplicate the current token
-        len = strlen(token);    // Length of the token
-        len2 = strlen(normie);  // Length of the duplicate
-
-        // Reverse the `token`
-        for (int i = 0; i < len / 2; i++) {
-            char temp = token[i];
-            token[i] = token[len - 1 - i];
-            token[len - 1 - i] = temp;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ') {
+            noSpaces[index++] = str[i];
         }
-
-        //Compare the reversed `token` with `normie`
-        for (int i = 0; i < len; i++) {
-            for (int j = len2; j > 0 ; j++) {
-                if (token[i] == normie[j]) {
-                    printf("Yes");
-                } else {
-                    printf("No");
-                }
-            }
-        }
-        // printf("\n");
-
-        free(normie); // Free the memory allocated by strdup
-        token = strtok(NULL, " "); // Move to the next token
+    }
+    noSpaces[index] = '\0'; 
+    
+    if (isPalindrome(noSpaces, index)) {
+        printf("Yes");
+    } else {
+        printf("No");
     }
 
     return 0;
