485137ae5e5cab7f70d1c2896766944cbe98b9b5
Tarunpreet Singh <141033706+GodXSpell@users.noreply.github.com>
2025-03-22 16:42:13 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

--- Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -4,14 +4,14 @@
 
 int main() {
     char str[100];
-    scanf("%[^\n]", str); // Read the input string including spaces
+    scanf("%[^\n]", str); 
 
-    char *token = strtok(str, " "); // Tokenize the string using space as the delimiter
-    char *normie; // To store the duplicate of the token
+    char *token = strtok(str, " "); 
+    char *normie; 
     int len, len2;
 
     while (token != NULL) {
-        printf("%s", token); // Print each token on a new line
+        //printf("%s", token); 
 
         normie = strdup(token); // Duplicate the current token
         len = strlen(token);    // Length of the token
@@ -24,16 +24,16 @@ int main() {
             token[len - 1 - i] = temp;
         }
 
-        // Compare the reversed `token` with `normie`
-        // for (int i = 0; i < len; i++) {
-        //     for (int j = 0; j < len2; j++) {
-        //         if (token[i] == normie[j]) {
-        //             printf("Yes");
-        //         } else {
-        //             printf("No");
-        //         }
-        //     }
-        // }
+        //Compare the reversed `token` with `normie`
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len2; j++) {
+                if (token[i] == normie[j]) {
+                    printf("Yes");
+                } else {
+                    printf("No");
+                }
+            }
+        }
         // printf("\n");
 
         free(normie); // Free the memory allocated by strdup
