e2741d8131da462417f2994043504bdd968f0f6b
Tarunpreet Singh <141033706+GodXSpell@users.noreply.github.com>
2025-03-28 17:37:25 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -10,8 +10,8 @@ int main(){
     }
 
     for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-            if (!(i <= j+1)){
+        for(int j=i+1;j<size;j++){
+            if (!(i <= j)){
                 yes = 0;
             }
         }
