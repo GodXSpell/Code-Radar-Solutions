7223f8340a0d53b67a84ff229e35ed0ec4477b83
Tarunpreet Singh <141033706+GodXSpell@users.noreply.github.com>
2025-03-31 22:28:35 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,10 @@
+// Your code here...
+#include <stdio.h>
+int fibonacciSeries(int n){
+    if (n = 1){
+        return 1;
+    }else{
+        return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    }
+    
+}
